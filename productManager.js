0eda7eb266c0a06ec7c664bfe726bdf8cb4d3621
Ivan Cortes <ivancb97@gmail.com>
2024-01-10 13:28:51 -0300

    [test] Se arregla problema al eliminar productos por id

--- productManager.js
+++ productManager.js
@@ -78,14 +78,17 @@ class ProductManager {
     }
 
     deleteProduct(id) {
-        this.products = this.products.filter((el) => el !== this.getProductById(id));
-        const data = JSON.stringify(this.products, null, 4);
-        fs.writeFile('./productos.json', data, 'utf-8')
-        .then(() => {
-            console.log(`Producto con ID ${id} eliminado correctamente.`)
+
+        //LEER ANTES CON FS Y DE AHI FILTRAR PORQUE SINO DEPENDE DE LA EJECUCION PORQUE LEE EL ARRAY.
+
+        const data = fs.readFile(this.path, 'utf-8')
+        .then((data) => {
+            this.products = JSON.parse(data);
+            this.products = this.products.filter((el) => el !== this.getProductById(id));
+            this.saveDataToFile()
         })
         .catch((err) => {
-            console.log(`Error al eliminar el producto con ID ${id}: ${err}`)
+            console.log(`No se pudo eliminar el producto. Error: ${err}`);
         })
     }
 }
@@ -128,4 +131,4 @@ productManager.addProduct(product2);
 // productManager.updateProduct(2, 'Monitor Curvo Samsung Odyssey G7 - 32 pulgadas, QLED, 240Hz, 1ms, G-Sync, FreeSync Premium Pro','Sumérgete en la acción con el monitor curvo Samsung Odyssey G7. Con una pantalla QLED de 32 pulgadas y una frecuencia de actualización de 240Hz junto con un tiempo de respuesta de 1ms, este monitor ofrece una experiencia de juego sin igual. La tecnología G-Sync y FreeSync Premium Pro garantizan imágenes fluidas y libres de tearing. El diseño curvo 1000R proporciona una inmersión completa en tus juegos favoritos y trabajos creativos. Además, cuenta con un elegante sistema de iluminación ambiental en la parte trasera.', 1299.99, 'https://cdn3.iconfinder.com/data/icons/education-209/64bus-vehicle-transport-school-128.png', 'SAMG7-32QLED', 25)
 
 // Delete product
-productManager.deleteProduct(2);
+// productManager.deleteProduct(2);
