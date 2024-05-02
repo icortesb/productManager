4d5e46ae6dcf1cd2193b254c616ba092318439f5
Ivan Cortes <ivancb97@gmail.com>
2024-05-02 20:38:41 -0300

    new file:   src/public/404.html 	modified:   src/routes/index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,6 +5,7 @@ import routerChat from './chat.routes.js';
 import routerSessions from './sessions.routes.js';
 import routerViews from './views.routes.js';
 import routerAuth from './auth.routes.js';
+import rootDir from '../utils/dirname.js';
 
 const router = new Router(); 
 
@@ -14,5 +15,9 @@ router.use('/chat', routerChat);
 router.use('/api/sessions', routerSessions);
 router.use('/', routerViews);
 router.use('/auth', routerAuth);
+// Usar * para mandar a un 404 lo que no encuentre
+router.use('*', (req, res) => {
+    res.status(404).sendFile('public/404.html', { root: rootDir });
+});
 
 export default router;
