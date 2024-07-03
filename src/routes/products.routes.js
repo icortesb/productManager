58a3c7bd36f3fbfa062120f9ac8be4c271cd0e11
Ivan Cortes <ivancb97@gmail.com>
2024-07-03 07:40:40 -0300

    modified:   src/public/css/style.css 	modified:   src/routes/products.routes.js 	modified:   src/views/login.handlebars

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -7,7 +7,7 @@ const productManager = new ProductManager();
 
 routerProducts.get('/', productManager.getProducts);
 routerProducts.get('/:id', productManager.getProductById);
-routerProducts.post('/', authRole(['premium', 'admin']), productManager.addProduct);
+routerProducts.post('/', authRole(['premium']), productManager.addProduct);
 routerProducts.put('/:id', authRole(['premium', 'admin']), productManager.updateProduct); 
 routerProducts.delete('/:id', authRole(['premium', 'admin']), productManager.deleteProduct); 
 export default routerProducts;
